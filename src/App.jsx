82aef162e8b0530bd4dc73e05a2f2cda042e238e
5ham <surajrxl06@gmail.com>
2026-03-22 16:49:30 +0530

    fixed a minor bug

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
     const data = await response.json()
     setShortUrl(domain_name + data.shortenedUrl)
     SetIsShort(true)
+    setUserUrl("")
   }
 
   const handleBack = () => {
